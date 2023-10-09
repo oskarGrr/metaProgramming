008bb5e9e7373f72868f4ebbc228b738c2d542b6
Grant <acdcgrr@gmail.com>
2023-10-09 01:55:16 -0500

    changed some formatting

--- BiDirectionalTypeMap/main.cpp
+++ BiDirectionalTypeMap/main.cpp
@@ -3,15 +3,20 @@
 
 typedef uint64_t index_t;
 
-template <index_t index, typename T> struct IntegerTypePair
+template <index_t index, typename T> 
+struct IntegerTypePair
 { 
     using type = T;
     static constexpr index_t integer = index;
-    static IntegerTypePair getPairFromIndex(std::integral_constant<index_t, integer>) {return {};}
+
+    static IntegerTypePair getPairFromIndex(
+        std::integral_constant<index_t, integer>) {return {};}
+
     static IntegerTypePair getPairFromType(T) {return {};}
 };
 
-template <typename ...IDTypePairs> struct BiDirectionalIndexTypeMap : IDTypePairs...
+template <typename ...IDTypePairs> 
+struct BiDirectionalIndexTypeMap : IDTypePairs...
 {
     using IDTypePairs::getPairFromIndex...;
     using IDTypePairs::getPairFromType...;
@@ -25,13 +30,17 @@ template <typename ...IDTypePairs> struct BiDirectionalIndexTypeMap : IDTypePair
         getPairFromType(T{}))::integer;
 };
 
-template <typename ...Ts> struct TypeRegistry
+template <typename ...Ts> 
+struct TypeRegistry
 {
     static constexpr size_t maxNumTypes{64};
     static constexpr size_t numTypes{sizeof...(Ts)};
-    static_assert(sizeof...(Ts) < maxNumTypes, "Exceeded maximum number of types");
 
-    template <index_t ...IDs> struct CombineWith 
+    static_assert(sizeof...(Ts) < maxNumTypes,
+        "Exceeded maximum number of types");
+
+    template <index_t ...IDs> 
+    struct CombineWith 
     { 
         static_assert(sizeof...(Ts) == sizeof...(IDs), 
             "There are mismatching Types to IDs in the TypeRegistry");
@@ -40,23 +49,29 @@ template <typename ...Ts> struct TypeRegistry
     };
 };
 
-template <size_t requestedID> 
+//Below are the two meta functions for a map that looks like:
+// <int, char, bool, float>
+// <0,   1,    2,    3    >
+
+//Get the type mapped to ID.
+template <size_t ID> 
 using GetType = 
 TypeRegistry <int, char, bool, float> ::
-CombineWith  <0,   1,    2,    3    > ::Map::type<requestedID>;
+CombineWith  <0,   1,    2,    3    > ::Map::type<ID>;
 
+//Get the ID mapped to T.
 template <typename T>
-static auto index = 
+static index_t getID = 
 TypeRegistry <int, char, bool, float> ::
 CombineWith  <0,   1,    2,    3    > ::Map::index<T>;
 
 int main()
 {
     std::cout << "1 is mapped to: " << typeid(GetType<1>).name() << '\n';
-    std::cout << "char is mapped to: " << index<char> << "\n\n";
+    std::cout << "char is mapped to: " << getID<char> << "\n\n";
 
     std::cout << "3 is mapped to: " << typeid(GetType<3>).name() << '\n';
-    std::cout << "bool is mapped to: " << index<bool> << '\n';
+    std::cout << "bool is mapped to: " << getID<bool> << '\n';
 
     return 0;
 }
